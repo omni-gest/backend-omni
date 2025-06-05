6be011cc2a33a05bdf63fc9d9d51d31f8fc3b1f4
Lucas Barreto <lucas_barreto29@outlook.com>
2025-06-05 19:58:41 -0300

    feat: fixed

--- app/Repositories/EstoqueRepository.php
+++ app/Repositories/EstoqueRepository.php
@@ -10,7 +10,6 @@ class EstoqueRepository implements EstoqueRepositoryInterface
 {
     public function create($request, $id_empresa)
     {
-        print('Criando estoque');
           $request['id_empresa_est'] = $id_empresa;
         $result = Estoque::create($request);
 
